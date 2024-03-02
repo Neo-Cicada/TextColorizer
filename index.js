adcb56d4af5e15d1fe02230051c6eb4ad0580676
Neo <lordneobarnachea@gmail.com>
2024-03-02 18:33:46 +0800

    add index as a test

---
+++ index.js
@@ -0,0 +1,17 @@
+const colorConsole = require('./colorConsole');
+
+colorConsole.red('This is red text');
+colorConsole.green('This is green text');
+colorConsole.blue('This is blue text');
+
+colorConsole.bgRed('This has a red background');
+colorConsole.bgGreen('This has a green background');
+colorConsole.bgBlue('This has a blue background');
+
+colorConsole.bold('This is bold text');
+colorConsole.underline('This is underlined text');
+
+colorConsole.reset('This resets the text style');
+colorConsole.shadow('Text with shadow effect');
+colorConsole.scroll('This is scrolling text!', 100);
+
